4b4f168312e7bcd5589c7a5eb5410809d9e42ff5
Karam Hussain <iqbalkaram44@gmail.com>
2024-09-27 06:23:38 +0500

    updated navbar code

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,41 +3,28 @@ import { discount, robot } from '../assets'
 import GetStarted from './GetStarted'
 
 const Hero = () => {
+  const newLocal = 
+  <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+    <div className='flex flex-row justify-between items-center w-full -mt-20 sm:-mt-32 lg:-mt-96'>
+      <h1 className='flex-1 font-poppins font-semibold ss:text-[55px] text-[55px] text-white ss:leading-[100.8px] leading-[75px]'>
+        Your Trusted <br className='sm:block hidden' /> {" "}
+        <span className='text-gradient'> Partner</span> {" "}
+      </h1>
+    </div>
+    <h1 className='font-poppins font-semibold ss:text-[44px] text-[44px] text-white ss:leading-[100px] leading-[75px] w-full'>
+      in Cloud Deployment.
+    </h1>
+    <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      Our team of experts uses a methodology to identify the credit cards most likely to fit your needs.
+      We examine annual percentage rates, annual fees.
+    </p>
+  </div>
   return (
     <section 
       id='home' 
       className={`flex md:flex-row flex-col ${styles.paddingY} -mt-26`} // Added negative margin-top
     >
-      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
-        <div className='mt-10 mb-2'>
-          {/* <img
-            src={discount}
-            alt='discount'
-            className='w-[32px] h-[32px]'
-          />
-          <p className={`${styles.paragraph} ml-2`}>
-            <span className='text-white'>20%</span> Discount For {" "}
-            <span className='text-white'>1 Month</span> Account
-          </p> */}
-        </div>
-        {/* <div className='flex flex-row justify-between items-center w-full -mt-40'> */}
-        <div className='flex flex-row justify-between items-center w-full -mt-36 sm:-mt-36 lg:-mt-96'>
-          <h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]'>
-            The Next <br className='sm:block hidden'/> {" "}
-            <span className='text-gradient'>Generation</span> {" "}
-          </h1>
-          <div className='ss:flex hidden md:mr-4 mr-0'>
-            <GetStarted/>
-          </div>
-        </div>
-        <h1 className='font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full'>
-          Payment Method.
-        </h1>
-        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-          Our team of experts uses a methodology to identify the credit cards most likely to fit your needs. 
-          We examine annual percentage rates, annual fees.  
-        </p>
-      </div>
+      {newLocal}
       <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
         <img
           src={robot}
